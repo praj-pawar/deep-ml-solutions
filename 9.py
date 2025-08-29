f7b527a31eccd2c1ec4c5931308b085eef0614b3
Prajwal Pawar <prajpawar23@gmail.com>
2025-08-29 15:15:22 +0530

    9.py

---
+++ 9.py
@@ -0,0 +1,12 @@
+import numpy
+
+
+def matrixmul(a: list[list[int | float]],
+              b: list[list[int | float]]) -> list[list[int | float]]:
+
+    A = np.array(a)
+    B = np.array(b)
+
+    if len(a[0]) != len(b)
+    return -1
+    return A @ B
