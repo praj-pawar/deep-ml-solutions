f48f1e34362d51bcf382060cad91e84659999c52
Prajwal Pawar <prajpawar23@gmail.com>
2025-09-02 22:06:08 +0530

    47.py

--- 15.py
+++ 15.py
@@ -5,8 +5,8 @@ def linear_regression_gradient_descent(X: np.ndarray, y: np.ndarray, alpha: floa
     m, n = X.shape
     theta = np.zeros((n, 1))
     for _ in range(iterations):
-        predictions = X @ theta
-        errors = predictions - y.reshape(-1, 1)
-        updates = X.T @ errors / m
-        theta -= alpha * updates
+        predictions = X @ theta  # mxn X nx1 = mx1
+        errors = predictions - y.reshape(-1, 1)  # mx1 - mx1 = mx1
+        updates = X.T @ errors / m  # nxm X mx1 = nx1
+        theta -= alpha * updates  # nx1 - nx1 = nx1
     return np.round(theta.flatten(), 4)
