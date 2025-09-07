93393f5ca58195c09034c916f81f67c2347a3330
Prajwal Pawar <prajpawar23@gmail.com>
2025-09-07 20:16:49 +0530

    75.py

--- 75.py
+++ 75.py
@@ -1,25 +1,11 @@
-
 from collections import Counter
 
 
 def confusion_matrix(data):
-    # Implement the function here
-    tot = len(data)
-    tp = 0
-    fp = 0
-    tn = 0
-    fn = 0
-
-    for i in data:
-        if i[1] == 1:
-            if i[0] == 1:
-                tp += 1
-            else:
-                fp += 1
-        elif i[1] == 0:
-            if i[0] == 0:
-                tn += 1
-            else:
-                fn += 1
-    return [[tp, fn], [fp, tn]]
-    pass
+    counts = Counter(tuple(pair) for pair in data)
+    # Get metrics
+    TP, FN, FP, TN = counts[(1, 1)], counts[(
+        1, 0)], counts[(0, 1)], counts[(0, 0)]
+    # Define matrix and return
+    confusion_matrix = [[TP, FN], [FP, TN]]
+    return confusion_matrix
