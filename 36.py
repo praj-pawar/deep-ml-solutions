a6b1656b66c42fcbaac333331c15487680d11e0f
Prajwal Pawar <prajpawar23@gmail.com>
2025-09-08 21:35:18 +0530

    36.py

--- 36.py
+++ 36.py
@@ -4,7 +4,7 @@ import numpy as np
 def accuracy_score(y_true, y_pred):
     # Your code here
     n = len(y_true)
-    c = 0
+    c = 0.0
     for i in range(0, n):
         if y_true[i] == y_pred[i]:
             c += 1
