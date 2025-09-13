6b20366898b205c0b967b92f10b39c9e182a315a
Prajwal Pawar <prajpawar23@gmail.com>
2025-09-14 03:01:51 +0530

    14.py

--- 14.py
+++ 14.py
@@ -5,6 +5,6 @@ def linear_regression_normal_equation(X: list[list[float]], y: list[float]) -> l
     X = np.array(X)
     y = np.array(y).reshape(-1, 1)
     X_transpose = X.T
-    theta = np.linalg.inv(X_transpose.dot(X)).dot(X_transpose).dot(y)
+    theta = np.linalg.inv(X.T @ X) @ (X.T) @ y
     theta = np.round(theta, 4).flatten().tolist()
     return theta
