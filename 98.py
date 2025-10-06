e20e36fa00f750639cc047abb349330566fde2bd
Prajwal Pawar <prajpawar23@gmail.com>
2025-10-06 20:19:59 +0530

    98.py

---
+++ 98.py
@@ -0,0 +1,15 @@
+def prelu(x: float, alpha: float = 0.25) -> float:
+	"""
+	Implements the PReLU (Parametric ReLU) activation function.
+
+	Args:
+		x: Input value
+		alpha: Slope parameter for negative values (default: 0.25)
+
+	Returns:
+		float: PReLU activation value
+	"""
+	# Your code here
+    if x >= 0:
+        return x
+    return alpha*x
